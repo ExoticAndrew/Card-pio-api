a0f3553d3bdd4168afff8306a4ec5d4cc96a7b9d
ExoticAndrew <andrewantunes61@gmail.com>
2025-06-23 15:37:26 -0300

    criação do repository

---
+++ src/main/java/vendas/Vendedor/Repository/Repository/foodRepository.java
@@ -0,0 +1,6 @@
+package vendas.Vendedor.Repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import vendas.Vendedor.Food.Food;
+
+public interface foodRepository extends JpaRepository<Food, Long> {
+}
