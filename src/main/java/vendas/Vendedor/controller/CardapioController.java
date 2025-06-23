af206f5c14ac072b0e1c6290f695fdae716c32af
ExoticAndrew <andrewantunes61@gmail.com>
2025-06-23 13:05:04 -0300

    Initial commit

---
+++ src/main/java/vendas/Vendedor/controller/CardapioController.java
@@ -0,0 +1,16 @@
+package vendas.Vendedor.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping ("/food")
+public class CardapioController {
+    @GetMapping
+    public void getALL(){
+
+    }
+}
