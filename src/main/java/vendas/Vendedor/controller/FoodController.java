c66175a30e2bbddbe7ad0f194a8701ceb484252e
ExoticAndrew <andrewantunes61@gmail.com>
2025-07-05 13:23:55 -0300

    refactor: clean up FoodController and add delete endpoint

--- src/main/java/vendas/Vendedor/controller/FoodController.java
+++ src/main/java/vendas/Vendedor/controller/FoodController.java
@@ -12,22 +12,29 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping ("/food")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/food")
 public class FoodController {
+
     @Autowired
     private foodRepository repository;
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
+
     @PostMapping
-    public void saveFood(@RequestBody FoodRequestDTO data){
+    public void saveFood(@RequestBody FoodRequestDTO data) {
         Food foodData = new Food(data);
         repository.save(foodData);
-        return;
-
     }
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
+
     @GetMapping
-    public List<FoodResponseDTO> getALL(){
-        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+    public List<FoodResponseDTO> getALL() {
+        List<FoodResponseDTO> foodList = repository.findAll().stream()
+                .map(FoodResponseDTO::new)
+                .toList();
         return foodList;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFood(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
